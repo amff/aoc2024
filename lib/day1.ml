297dc7d0e549e6bcb6697c2966009309fef7c365
André Faria <andre.faria@vortal.biz>
2024-12-05 16:18:38 +0000

    Day2 part1

--- lib/day1.ml
+++ lib/day1.ml
@@ -1,24 +1,45 @@
 open! Imports
+open Base
 
 module M = struct
   (* Type to parse the input into *)
-  type t = unit
+  type t = int list * int list
 
   (* Parse the input to type t, invoked for both parts *)
-  let parse _inputs = ()
+  let parse _inputs =
+    String.split_lines _inputs
+    |> List.map ~f:(fun line ->
+           Stdlib.Scanf.sscanf line "%d %d" (fun i1 i2 -> (i1, i2)) )
+    |> Stdlib.List.split
 
   (* Run part 1 with parsed inputs *)
-  let part1 _ = ()
+  let part1 (first, second) =
+    let sorted_first = List.sort first ~compare:Int.compare
+    and sorted_second = List.sort second ~compare:Int.compare in
+    let rec s_aux l1 l2 acc =
+      match (l1, l2) with
+      | [], [] -> acc
+      | [], _ | _, [] -> failwith "invalid input"
+      | i1 :: t1, i2 :: t2 -> s_aux t1 t2 (acc + abs (i1 - i2))
+    in
+    Stdio.print_endline
+    @@ (s_aux sorted_first sorted_second 0 |> Int.to_string)
 
   (* Run part 2 with parsed inputs *)
-  let part2 _ = ()
+  let part2 (first, second) =
+    List.map first ~f:(fun x -> x * List.count second ~f:(fun i -> i = x))
+    |> List.fold ~init:0 ~f:Int.( + )
+    |> Int.to_string |> Stdio.print_endline
 end
 
 include M
 include Day.Make (M)
 
 (* Example input *)
-let example = ""
+let example = "3 5\n5 1"
 
 (* Expect test for example input *)
-let%expect_test _ = run example ; [%expect {| |}]
+let%expect_test _ = run example ; [%expect {| 
+    2 
+    5 
+      |}]
--- lib/day2.ml
+++ lib/day2.ml
@@ -1,14 +1,42 @@
 open! Imports
+open Base
 
 module M = struct
   (* Type to parse the input into *)
-  type t = unit
+  type t = int list list
+
+  type direction = Ascending | Descending
 
   (* Parse the input to type t, invoked for both parts *)
-  let parse _inputs = ()
+  let parse _inputs =
+    String.split_lines _inputs
+    |> List.map ~f:(fun line ->
+           String.split line ~on:' ' |> List.map ~f:Int.of_string )
+
+  let rec s_aux (last_n, dir) rst =
+    match rst with
+    | [] -> true
+    | fst :: tl ->
+        let diff = fst - last_n in
+        if diff > 0 && diff < 4 && phys_equal dir Ascending then
+          s_aux (fst, dir) tl
+        else if diff < 0 && diff > -4 && phys_equal dir Descending then
+          s_aux (fst, dir) tl
+        else false
+
+  let is_report_safe = function
+    | fst :: snd :: tl ->
+        let diff = snd - fst in
+        if diff > 0 && diff < 4 then s_aux (snd, Ascending) tl
+        else if diff < 0 && diff > -4 then s_aux (snd, Descending) tl
+        else false
+    | _ -> failwith "invalid report"
 
   (* Run part 1 with parsed inputs *)
-  let part1 _ = ()
+  let part1 input =
+    let report_safety_list = List.map input ~f:is_report_safe in
+    Stdio.print_endline
+    @@ (List.count report_safety_list ~f:Fn.id |> Int.to_string)
 
   (* Run part 2 with parsed inputs *)
   let part2 _ = ()
@@ -18,7 +46,9 @@ include M
 include Day.Make (M)
 
 (* Example input *)
-let example = ""
+let example =
+  "7 6 4 2 1\n1 2 7 8 9\n9 7 6 2 1\n1 3 2 4 5\n8 6 4 4 1\n1 3 6 7 9"
 
 (* Expect test for example input *)
-let%expect_test _ = run example ; [%expect {| |}]
+let%expect_test _ = run example ; [%expect {| 2 
+        4 |}]
