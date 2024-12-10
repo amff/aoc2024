c7ecd1c94be91cbd42139daf294917388c38a31a
Andre Faria <andre.faria@vortal.biz>
2024-12-10 00:49:13 +0000

    Day9 part2 wip

--- lib/day9.ml
+++ lib/day9.ml
@@ -3,9 +3,11 @@ open Base
 
 module M = struct
   (* Type to parse the input into *)
-  type t = int option list
+  type e = File of int * int | Gap | Free
 
-  type n = Gap of int | File of int * int
+  type t = e Array.t
+
+  let is_file e = match e with File (_, _) -> true | _ -> false
 
   (* Parse the input to type t, invoked for both parts *)
   let parse _inputs =
@@ -15,54 +17,50 @@ module M = struct
       | elem :: rst ->
           let repeat_n = Char.get_digit_exn elem in
           if not is_free_space then
-            List.init repeat_n ~f:(fun _ -> Some cur_id)
+            List.init repeat_n ~f:(fun _ -> File (cur_id, repeat_n))
             :: go rst (not is_free_space) cur_id
           else
-            List.init repeat_n ~f:(fun _ -> None)
+            List.init repeat_n ~f:(fun _ -> Gap)
             :: go rst (not is_free_space) (cur_id + 1)
     in
-    go (String.to_list _inputs) false 0 |> List.concat
+    go (String.to_list _inputs) false 0 |> List.concat |> Array.of_list
 
-  let parse2 inputs =
-    let rec go lst is_free_space cur_id =
-      match lst with
-      | [] -> []
-      | elem :: rst ->
-          if not is_free_space then
-            File (cur_id, elem) :: go rst (not is_free_space) cur_id
-          else Gap elem :: go rst (not is_free_space) (cur_id + 1)
-    in
-    go inputs false 0
+  let check_sum =
+    Array.foldi ~init:0 ~f:(fun idx acc c ->
+        match c with File (c, _) -> acc + (c * idx) | _ -> acc )
 
-  let check_sum fs_list =
-    List.foldi fs_list ~init:0 ~f:(fun idx acc c_opt ->
-        match c_opt with None -> acc | Some c -> acc + (c * idx) )
+  let compact fsa =
+    let front_idx = ref 0 and back_idx = ref (Array.length fsa - 1) in
+    while !front_idx < !back_idx do
+      let front_elem = Array.get fsa !front_idx
+      and back_elem = Array.get fsa !back_idx in
+      match (front_elem, back_elem) with
+      | File _, _ ->
+          front_idx := !front_idx + 1 (* front index forward until gap *)
+      | Gap, Gap ->
+          back_idx :=
+            !back_idx - 1 (* back index back until letter to defrag *)
+      | Gap, File (id, len) ->
+          Array.set fsa !front_idx (File (id, len)) ;
+          Array.set fsa !back_idx Free ;
+          front_idx := !front_idx + 1 ;
+          back_idx := !back_idx - 1
+      | _ -> failwith "unreachable"
+    done
 
-  let defrag fs_list =
-    let () = Stdlib.print_endline "Start defrag" in
-    let a = Array.of_list fs_list
-    and front_idx = ref 0
-    and back_idx = ref (List.length fs_list - 1) in
-    let () =
-      while !front_idx < !back_idx do
-        let front_elem = Array.get a !front_idx
-        and back_elem = Array.get a !back_idx in
-        match (front_elem, back_elem) with
-        | Some _, _ ->
-            front_idx := !front_idx + 1 (* front index forward until gap *)
-        | None, None ->
-            back_idx :=
-              !back_idx - 1 (* back index back until letter to defrag *)
-        | None, Some n ->
-            Array.set a !front_idx (Some n) ;
-            Array.set a !back_idx None (* swap *)
-      done
-    in
-    Array.to_list a
+  let rec gap_from arr n =
+    match Array.get arr n with
+    | Free | Gap -> 1 + (gap_from arr n + 1)
+    | _ -> 0
+
+  let defrag fsa =
+    let files = Array.filter fsa ~f:is_file in
+    _
 
   (* Run part 1 with parsed inputs *)
   let part1 input =
-    defrag input |> check_sum |> Stdlib.print_int |> Stdlib.print_newline
+    compact input ;
+    check_sum input |> Stdlib.print_int |> Stdlib.print_newline
 
   (* Run part 2 with parsed inputs *)
   let part2 _ = ()
